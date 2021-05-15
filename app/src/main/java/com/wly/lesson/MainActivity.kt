f9211725e07546b493e821699d572ae933ca3b53
wanglyGithub <wlytctw@163.com>
2021-05-15 17:25:36 +0800

    Dev: 新建kotlinLib 库

--- app/src/main/java/com/wly/lesson/MainActivity.kt
+++ app/src/main/java/com/wly/lesson/MainActivity.kt
@@ -3,7 +3,9 @@ package com.wly.lesson
 import com.wly.baselib.base.KTBaseActivity
 import com.wly.baselib.utils.UILogUtil
 import com.wly.lesson.contract.TestConstract
+import com.wly.lesson.dsl.customTextWatch
 import com.wly.lesson.mvp.KTMainPresenter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : KTBaseActivity<KTMainPresenter, TestConstract.View>(), TestConstract.View {
     override fun initPresenter(): KTMainPresenter = KTMainPresenter()
@@ -13,6 +15,13 @@ class MainActivity : KTBaseActivity<KTMainPresenter, TestConstract.View>(), Test
     override fun initView() {
         super.initView()
         mPresenter?.request(map = HashMap())
+
+        //dsl
+        edit.addTextChangedListener(customTextWatch {
+            afterTextChanged {
+
+            }
+        })
     }
 
     override fun showData(data: String) {
---
+++ kotlinLib/src/main/java/com/wly/kotlinlib/dsl/TextChangedListenerDsl.kt
@@ -0,0 +1,53 @@
+package com.wly.kotlinlib.dsl
+
+import android.text.Editable
+import android.text.TextWatcher
+
+/**
+ * author: wanglyGitHub
+ * date: 2021-05-15
+ * description:DSL 方式 简化 editText 监听
+ */
+private typealias afterTextChanged = (p0: Editable?) -> Unit
+
+private typealias beforeTextChanged = (p0: CharSequence?, p1: Int, p2: Int, p3: Int) -> Unit
+private typealias onTextChanged = (p0: CharSequence?, p1: Int, p2: Int, p3: Int) -> Unit
+
+class CustomTextWatcherDsl : TextWatcher {
+
+    private var afterTextChanged: afterTextChanged? = null
+    private var beforeTextChanged: beforeTextChanged? = null
+    private var onTextChanged: onTextChanged? = null
+
+    override fun afterTextChanged(p0: Editable?) {
+
+        return afterTextChanged?.invoke(p0) ?: Unit
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        return beforeTextChanged?.invoke(p0, p1, p2, p3) ?: Unit
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        return onTextChanged?.invoke(p0, p1, p2, p3) ?: Unit
+    }
+
+
+    fun afterTextChanged(afterTextChanged: afterTextChanged) {
+        this.afterTextChanged  = afterTextChanged
+
+    }
+
+    fun beforeTextChanged(beforeTextChanged: beforeTextChanged) {
+        this.beforeTextChanged = beforeTextChanged
+
+    }
+
+    fun onTextChanged(onTextChanged: onTextChanged) {
+        this.onTextChanged = onTextChanged
+
+    }
+
+}
+
+fun customTextWatch(watcher: CustomTextWatcherDsl.()->Unit) = CustomTextWatcherDsl().also(watcher)
